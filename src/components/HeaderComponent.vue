b852a6c727a00493aff7736c263fb987637cb464
福飯　雄太 <yuta@fukuhanyuutanoMacBook-Air.local>
2022-02-26 16:04:42 +0900

    feat: 画面の作成

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  header
+    p トークテーマ
+</template>
+
+<style scoped lang="scss">
+  header {
+    width: 100%;
+    background: #fff;
+    border-bottom: 1px solid #dfe5ea;
+    box-shadow: 0 3px 16px #dfe5ea;
+    position: fixed;
+    z-index: 200;
+    p {
+      width: 100%;
+      padding: 20px 0 20px 20px;
+      margin-bottom: 0;
+      color: #313131;
+    }
+  }
+</style>
